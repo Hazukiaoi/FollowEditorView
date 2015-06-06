01facec5460c00beb2f797ccad9e3bf2aa1953a3
hazukiaoi <hazuki.aoi1@gmail.com>
2015-06-06 11:13:01 +0900

    SetCameraFollowEditorView
    SetCameraFollowEditorView
    1.0

---
+++ FollowEditorView.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[ExecuteInEditMode]
+public class FollowEditorView : MonoBehaviour {
+#if UNITY_EDITOR
+    Camera cam;
+    public void OnDrawGizmos()
+    {
+        cam = GetComponent<Camera>();
+        transform.localPosition = SceneView.currentDrawingSceneView.camera.transform.localPosition;
+        transform.localRotation = SceneView.currentDrawingSceneView.camera.transform.localRotation;
+        cam.orthographic = SceneView.currentDrawingSceneView.camera.orthographic;
+        if (cam.orthographic)
+        {
+            cam.orthographicSize = SceneView.currentDrawingSceneView.camera.orthographicSize;
+        }
+        else
+        {
+            cam.fieldOfView = SceneView.currentDrawingSceneView.camera.fieldOfView;
+        }
+    }
+#endif
+}
